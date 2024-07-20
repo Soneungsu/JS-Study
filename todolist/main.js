69a1b2711f3cd713dee64276cc6f121b62fbfa59
eungsu <eungsu1217@gmail.com>
2024-07-20 19:24:36 +0900

    todolist add if task done and notdone

--- todolist/main.js
+++ todolist/main.js
@@ -6,19 +6,57 @@
 
 const addBtn = document.querySelector(".add-btn");
 let userValue = document.querySelector("#user-value");
+let taskList = [];
+
+const addTask = () => {
+  let userTask = userValue.value;
+  let task = {
+    id: newID(),
+    taskContent: userTask,
+    isComplete: false,
+  };
+  taskList.push(task);
+  console.log(taskList);
+  render();
+};
+
+const doneBtn = (id) => {
+  let task = taskList.find((item) => item.id === id);
+  if (task) {
+    task.isComplete = !task.isComplete;
+  }
+  console.log(task);
+  render();
+};
 
 const render = () => {
-  let userTask = userValue.value;
   //   console.log(userTask);
-  let resultHTML = "";
-  resultHTML += `  <div class="item-list">
-            <div class="item">${userTask}</div>
-            <div>
-              <button id="done-btn">완료</button>
-              <button id="delete-btn">삭제</button>
-            </div>
-          </div>`;
+  let resultHTML = taskList
+    .map((item, index) => {
+      if (item.isComplete === false) {
+        return `<div class="item-list">
+          <div class="item">${item.taskContent}</div>
+          <div>
+            <button id="done-btn" data-index="${index}" onclick="doneBtn('${item.id}')">완료</button>
+            <button id="delete-btn" data-index="${index}">삭제</button>
+          </div>
+        </div>`;
+      } else {
+        return `<div class="item-list">
+          <div class="item" id="task-done">${item.taskContent}</div>
+          <div>
+            <button id="done-btn" data-index="${index}" onclick="doneBtn('${item.id}')">완료</button>
+            <button id="delete-btn" data-index="${index}">삭제</button>
+          </div>
+        </div>`;
+      }
+    })
+    .join();
   document.querySelector(".main-board").innerHTML = resultHTML;
 };
 
 addBtn.addEventListener("click", addTask);
+
+var newID = function () {
+  return Math.random().toString(36).substr(2, 16);
+};
